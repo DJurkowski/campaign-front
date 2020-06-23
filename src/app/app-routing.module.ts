a9dcd539d177f77f3ff0f2ef622df750d4b6f6ac
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-06-23 15:50:20 +0200

    Added path for main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProjectsListComponent } from './views/projects-list/projects-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProjectsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
