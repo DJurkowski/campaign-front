d8da7855b8d4ec7641aad1c3ddea2fad61d54aec
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-06-23 18:55:06 +0200

    Added path to new-project component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProjectsListComponent } from './views/projects-list/projects-list.component';
+import { NewProjectComponent } from './views/new-project/new-project.component';
 
 const routes: Routes = [
-  { path: '', component: ProjectsListComponent }
+  { path: '', component: ProjectsListComponent },
+  { path: 'new-project', component: NewProjectComponent },
 ];
 
 @NgModule({
