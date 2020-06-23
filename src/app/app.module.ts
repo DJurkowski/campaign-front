308e37879607835598086caffee54da3e8e99a76
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-06-23 18:55:45 +0200

    Added new project component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsListComponent } from './views/projects-list/projects-list.component';
+import { NewProjectComponent } from './views/new-project/new-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsListComponent
+    ProjectsListComponent,
+    NewProjectComponent
   ],
   imports: [
     BrowserModule,
