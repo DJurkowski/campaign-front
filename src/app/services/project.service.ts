1a2988538e583048d8d64cc9e7744836c840d922
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-06-25 12:15:10 +0200

    Added crud requests for project and campaign

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { WebRequestService } from './web-request.service';
+import { Observable } from 'rxjs';
+import { Campaign } from './../models/campaign.module';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  constructor(private webRequestService: WebRequestService) { }
+
+  createProject(title: string): Observable<any> {
+    return this.webRequestService.post('/projects', { title });
+  }
+
+  editProject(id: string, title: string): Observable<any> {
+    return this.webRequestService.patch(`/projects/${id}`, {title});
+  }
+
+  getProjects(): Observable<any> {
+    return this.webRequestService.get('/projects');
+  }
+
+  deleteProject(id: string) {
+    return this.webRequestService.delete(`/projects/${id}`);
+  }
+
+  createCampaign(projectId: string, campaign: Campaign): Observable<any> {
+    return this.webRequestService.post(`/projects/${projectId}/campaign`, campaign);
+  }
+
+  editCampaign(projectId: string, campaignId: string, campaign: Campaign): Observable<any> {
+    return this.webRequestService.patch(`/projects/${projectId}/campaign/${campaignId}`, campaign );
+  }
+
+  getCampaign(projectId: string): Observable<any> {
+    return this.webRequestService.get(`/projects/${projectId}/campaign`);
+  }
+
+  deleteCampaign(projectId: string, campaignId: string) {
+    return this.webRequestService.delete(`/projects/${projectId}/campaign/${campaignId}`);
+  }
+
+}
