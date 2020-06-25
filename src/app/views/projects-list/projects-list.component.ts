0849225b48c504a0967581fba96a5d01a241b735
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-06-25 15:04:32 +0200

    Added displaying campaign for project

--- src/app/views/projects-list/projects-list.component.ts
+++ src/app/views/projects-list/projects-list.component.ts
@@ -1,4 +1,8 @@
+import { ProjectService } from './../../services/project.service';
 import { Component, OnInit } from '@angular/core';
+import { Campaign } from './../../models/campaign.module';
+import { Project } from './../../models/project.module';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-projects-list',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsListComponent implements OnInit {
 
-  constructor() { }
+  projects: Project[];
+  campaigns: Campaign[];
 
-  ngOnInit(): void {
+  selectedProjectId: string;
+
+  constructor(private projectService: ProjectService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        if (params.projectId) {
+          this.selectedProjectId = params.projectId;
+          this.projectService.getCampaign(params.projectId).subscribe((campaign: Campaign[]) => {
+            this.campaigns = campaign;
+            console.log(campaign);
+          });
+        } else {
+          this.campaigns = undefined;
+        }
+      }
+    );
+
+    this.projectService.getProjects().subscribe((projects: any[]) => {
+      this.projects = projects;
+    });
   }
 
 }
